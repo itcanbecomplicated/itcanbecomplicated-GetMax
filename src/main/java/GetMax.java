f04b02c5ebef96855de103312d7a0108c804e591
Nikita <itcanbecomplicatedinfo@gmail.com>
2023-02-12 20:09:19 +0000

    challenge5.4

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,12 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int maxVal=arr[0];
+        for(int i=1; i<arr.length; i++) {
+            if(maxVal<arr[i]) {
+                maxVal=arr[i];
+            } 
+        }
+        return maxVal;
     }
 }
